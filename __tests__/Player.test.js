2f47143beebec7e2f372fe34921a5769da7cb28e
kierstenv <kierstenvicknair22@gmail.com>
2022-03-14 08:49:07 -0500

    player's health methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -3,6 +3,7 @@ const Potion = require('../lib/Potion');
 jest.mock('../lib/Potion');
 
 const Player = require('../lib/Player');
+const { expect } = require('@jest/globals');
 
 test('creates a player object', () => {
   const player = new Player('kier');
@@ -30,8 +31,39 @@ test('gets inventory from player or returns false', () => {
   const player = new Player('kier');
   
   expect(player.getInventory()).toEqual(expect.any(Array));
-
+  
   player.inventory = [];
-
+  
   expect(player.getInventory()).toEqual(false);
 });
+
+test("gets player's health value", () => {
+  const player = new Player('kier');
+
+  expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('checks if player is alive or not', () => {
+  const player = new Player('kier');
+
+  expect(player.isAlive()).toBeTruthy();
+
+  player.health = 0;
+
+  expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+  const player = new Player('kier');
+  const oldHealth = player.health;
+  
+  player.reduceHealth(5);
+
+  expect(player.health).toBe(oldHealth - 5);
+  
+  player.reduceHealth(99999);
+
+  expect(player.health).toBe(0);
+});
+
+const player = new Player('kier');
